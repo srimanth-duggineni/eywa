f9cac9893aa144a09ff9e7d5cbde83b07cdbb513
srimanth-duggineni <srimanth6.duggineni@gmail.com>
2023-09-03 12:23:23 +1000

    tf: add vpc and its related resources

---
+++ infrastructure/environments/prod/variables.tf
@@ -0,0 +1,14 @@
+variable "account" {
+  description = "AWS Account"
+  type    = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "environment" {
+  description = "AWS Environment"
+  type        = string
+}
---
+++ infrastructure/environments/prod/vpc.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = "eywa-private"
+  cidr = "13.0.0.0/16"
+
+  azs             = ["ap-southeast-4a", "ap-southeast-4b"]
+  private_subnets = ["13.0.1.0/24"]
+  public_subnets  = ["13.0.2.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = var.environment
+  }
+}
