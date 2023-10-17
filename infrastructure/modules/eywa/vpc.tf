b2adcf0efd521fdfcb82c40f822621cc1cc5f82e
srimanth-duggineni <srimanth6.duggineni@gmail.com>
2023-10-17 17:03:59 +0530

    tf: works

--- infrastructure/modules/eywa/vpc.tf
+++ infrastructure/modules/eywa/vpc.tf
@@ -6,8 +6,8 @@ module "vpc" {
   cidr = "13.0.0.0/16"
 
   azs             = ["ap-southeast-2a", "ap-southeast-2b"]
-  private_subnets = ["13.0.2.0/24", "13.0.3.0/24"]
-  public_subnets  = ["13.0.1.0/24"]
+  private_subnets = ["13.0.3.0/24", "13.0.4.0/24"]
+  public_subnets  = ["13.0.1.0/24", "13.0.2.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
